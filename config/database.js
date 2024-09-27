57196632d1edcf4806c8852de07715aa01bd5c1a
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2024-09-27 18:39:58 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const chalk = require('chalk');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '12345',
+    password: '',                                              //password
     database: 'users_data'                                    // Combined database
 });
 
